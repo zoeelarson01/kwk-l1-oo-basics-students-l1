2f95d412b4d41ffaeac64bf8beac6db04e6d1aa3
zoeelarson01 <zoeelarson@gmail.com>
2018-06-07 22:21:34 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 # book.rb
 # Add you Book class here
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize (title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
+book = Book.new("Emma")
+book.turn_page
